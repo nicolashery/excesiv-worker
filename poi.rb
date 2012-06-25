3d0c2ea0af79461e14143ba8abc58bbf3b8e31f3
Nicolas Hery <nicolahery@gmail.com>
2012-06-25 16:59:58 -0400

    Add Apache POI Excel jars

---
+++ poi.rb
@@ -0,0 +1,10 @@
+# Wrapper around selected classes of the Apache POI Excel library
+
+require 'java'
+require 'require_all'
+# Require Apache POI jars
+require_rel 'lib/poi/*.jar'
+
+module Poi
+  XSSFWorkbook = Java::OrgApachePoiXssfUsermodel::XSSFWorkbook
+end
